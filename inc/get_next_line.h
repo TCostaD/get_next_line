4d16fe4e45549d5aa90c3f3129cd78debdcd3e3a
TCostad <tadeu.costa47@gmail.com>
2023-06-27 18:31:08 +0100

    mexi na biblioteca

--- inc/get_next_line.h
+++ inc/get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 18:17:53 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/06/22 19:03:33 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/06/27 17:55:34 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
