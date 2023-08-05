de71e2b4a7ab0878f3025413a3487619fcc7d4cb
TCostad <tadeu.costa47@gmail.com>
2023-08-05 14:33:30 +0100

    as funções q vao ser usadas precisam ficar antes

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,26 +6,11 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 18:17:51 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/08/05 14:27:19 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/08/05 14:33:11 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/get_next_line.h"
-
-char	*get_next_line(int fd)
-{
-	static char	*st_line;
-	char		*line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &st_line, 0) < 0)
-		return (0);
-	st_line = reader(st_line, fd);
-	if (!st_line)
-		return (NULL);
-	line = get_line(st_line);
-	st_line = trim(st_line);
-	return (line);
-}
 
 char	*reader(char *st_line, int fd)
 {
@@ -98,4 +83,19 @@ char	*trim(char *st_line)
 	tmp[j] = '\0';
 	free(st_line);
 	return (tmp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*st_line;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &st_line, 0) < 0)
+		return (0);
+	st_line = reader(st_line, fd);
+	if (!st_line)
+		return (NULL);
+	line = get_line(st_line);
+	st_line = trim(st_line);
+	return (line);
 }
